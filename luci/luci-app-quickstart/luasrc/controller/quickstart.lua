34340de2ae45138b66b40e76a060c5ee3cc8b291
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-06-04 15:08:20 +0800

    Update quickstart.lua

--- luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+++ luci/luci-app-quickstart/luasrc/controller/quickstart.lua
@@ -5,7 +5,7 @@ module("luci.controller.quickstart", package.seeall)
 function index()
     if luci.sys.call("pgrep quickstart >/dev/null") == 0 then
         local uci = require "luci.model.uci".cursor()
-        entry({"admin", "quickstart"}, template("quickstart/home"), _("QuickStart"), 1).leaf = true
+        entry({"admin", "quickstart"}, template("quickstart/home")).leaf = true
         entry({"admin", "network_guide"}, call("networkguide_index"), _("NetworkGuide"), 2)
         entry({"admin", "network_guide", "pages"}, call("quickstart_index", {index={"admin", "network_guide", "pages"}})).leaf = true
         if uci:get("quickstart", "main", "wifi_menu") == "1" then
